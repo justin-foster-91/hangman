25d96a24b1b5bc3b7db19818f0745820fa6e7621
Justin Foster <jfoster91@gmail.com>
2021-01-05 14:15:14 -0700

    Wireframing future functions

---
+++ hangman.js
@@ -0,0 +1,40 @@
+let numberOfGuesses = 0;
+let targetWord = '';
+let lettersGuessed = '';
+
+function renderStage(){
+
+}
+
+// external dictionary library?
+function targetWordIsWord(){
+
+}
+
+function inputTargetWord(){
+
+}
+
+function inputLetterGuess(){
+  // maximum guess limit
+}
+
+function fillWordSpaces(){
+
+}
+
+function fillIncorrectLetterBar(){
+
+}
+
+function renderGuessField(){
+  
+}
+
+function renderPerson(){
+
+}
+
+function playGame(){
+
+}
