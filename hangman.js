d45293391b4995cf73ae21605f156eeb4fe91853
Justin Foster <jfoster91@gmail.com>
2021-01-06 16:33:42 -0700

    Game functions through game over conditions

--- hangman.js
+++ hangman.js
@@ -1,40 +1,70 @@
 let numberOfGuesses = 0;
 let targetWord = '';
-let lettersGuessed = '';
+let secretTargetWord = '';
+let lettersGuessed = [];
+let wordBank = [
+  'hello',
+  'world'
+]
 
-function renderStage(){
-
+function startGame(){
+  targetWord = wordBank[Math.floor(Math.random() * wordBank.length)];
 }
 
-// external dictionary library?
-function targetWordIsWord(){
-
+function inputLetterGuess(letter){
+  lettersGuessed.push(letter.toLowerCase());
+  numberOfGuesses++;
 }
 
-function inputTargetWord(){
-
+function updateSecret(){
+  secretTargetWord = targetWord.split('').map(char => (lettersGuessed.includes(char)) ? char : '_').join(' ')
 }
 
-function inputLetterGuess(){
-  // maximum guess limit
+function isGameOver(){
+  if (numberOfIncorrectGuesses() > 6) return 'Lose';
+  if (!secretTargetWord.includes('_')) return 'Win';
 }
 
-function fillWordSpaces(){
-
+function numberOfIncorrectGuesses(){
+  return lettersGuessed.filter((char) => !targetWord.includes(char)).length
 }
 
-function fillIncorrectLetterBar(){
-
+function lossProgress(){
+  return Math.min(numberOfIncorrectGuesses() / 6, 1);
 }
 
-function renderGuessField(){
-  
+function display(){
+  console.log(secretTargetWord)
 }
 
-function renderPerson(){
+function gameLoop(){
+  startGame();
+  updateSecret();
+  while (!isGameOver()){
+    // Print game
+    display();
+    // Get input from user
+    const prompt = require('prompt-sync')();
+    let playerMove = prompt('What letter would you like to guess?');
+    if (playerMove === 'break') break;
 
+    inputLetterGuess(playerMove);
+    updateSecret();
+  }
+  display();
+  console.log(isGameOver());
 }
 
-function playGame(){
+gameLoop();
 
+function playGame(letter){
+  inputLetterGuess(letter);
+  updateSecret();
 }
+
+// Only count each letter once
+// Visual for progress bar
+// Display incorrect word bank
+// Restart button
+// title
+// instructions
